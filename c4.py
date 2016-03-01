2b170eff73bc523a56a3896d6cc37d5c55c04710
Alex Knight <alexk4096@gmail.com>
2016-03-01 11:38:32 +1100

    Add solution to Challenge 4

---
+++ c4.py
@@ -0,0 +1,15 @@
+from mcc_helpers import hex_string_to_bytearray
+from mcc_helpers import decrypt_sb_xor
+
+def c4(filename):
+	this_file = open(filename)
+	hex_strings = [x.rstrip('\n') for x in this_file.readlines()]
+	bytearrays = [hex_string_to_bytearray(x) for x in hex_strings]
+
+	candidates = [decrypt_sb_xor(x) for x in bytearrays]
+	best_candidate = max(candidates, key = lambda x: x[1])
+	(ba_dec, _) = best_candidate
+	return ba_dec.decode("utf-8")
+
+if __name__ == "__main__":
+	print c4('./4.txt')
